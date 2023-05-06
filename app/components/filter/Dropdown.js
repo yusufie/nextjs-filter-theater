b99c8a55d791516f40cce32f1376d6160eaafc9d
yusuf <yusufpirejman@gmail.com>
2023-05-06 22:50:59 +0300

    refactor: dropdown

--- app/components/filter/Dropdown.js
+++ app/components/filter/Dropdown.js
@@ -30,41 +30,41 @@ const Dropdown = () => {
       </button>
 
       {isOpen && (
-        <div className=" absolute left-0 z-10 w-72 py-2 mt-1 bg-white rounded-md shadow-md">
+        <div className="absolute left-0 z-10 w-72 py-2 mt-1 rounded-md shadow-md" id='dropdownMenu'>
         <h4 className="text-base font-bold text-black px-8 mt-8">Etkinlik Mekanı</h4>
         <ul>
 
           <li>
             <div className='px-8 py-2 hover:bg-gray-100'>
-              <input type='checkbox' className="w-5 h-5 text-pink-500"/>
+              <input type='checkbox' className="w-5 h-5 "/>
               <label className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Maximum Uniq Hall</label>
             </div>
           </li>
 
           <li>
             <div className='px-8 py-2 hover:bg-gray-100'>
-              <input type='checkbox' className="w-5 h-5 text-pink-500"/>
+              <input type='checkbox' className="w-5 h-5 "/>
               <label className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Maximum Uniq Box</label>
             </div>
           </li>
 
           <li>
             <div className='px-8 py-2 hover:bg-gray-100'>
-              <input type='checkbox' className="w-5 h-5 text-pink-500"/>
+              <input type='checkbox' className="w-5 h-5 "/>
               <label className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Maximum Uniq Lounge</label>
             </div>
           </li>
 
           <li>
             <div className='px-8 py-2 hover:bg-gray-100'>
-              <input type='checkbox' className="w-5 h-5 text-pink-500"/>
+              <input type='checkbox' className="w-5 h-5 "/>
               <label className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Maximum Uniq Açıkhava</label>
             </div>
           </li>
 
           <li>
             <div className='px-8 py-2 hover:bg-gray-100'>
-              <input type='checkbox' className="w-5 h-5 text-pink-500"/>
+              <input type='checkbox' className="w-5 h-5 "/>
               <label className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Bahçe Fuaye</label>
             </div>
           </li>
@@ -76,13 +76,13 @@ const Dropdown = () => {
         <ul>
           <li>
               <div className='px-8 py-2 hover:bg-gray-100'>
-                <input type='checkbox' className="w-5 h-5 text-pink-500"/>
+                <input type='checkbox' className="w-5 h-5 "/>
                 <label className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Güncel Etkinlikler</label>
               </div>
             </li>
             <li>
               <div className='px-8 py-2 hover:bg-gray-100'>
-                <input type='checkbox' className="w-5 h-5 text-pink-500"/>
+                <input type='checkbox' className="w-5 h-5 "/>
                 <label className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Geçmiş Etkinlikler</label>
               </div>
             </li>
